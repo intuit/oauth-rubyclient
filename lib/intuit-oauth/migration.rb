0e951834d0a845c853846c62bdd06bbb3167ff2d
Alexander Belonogov <belonogov@artistsoft.com>
2019-08-01 17:03:06 +0300

    Added missing `to_json` and fixed typo in the documentation.

--- lib/intuit-oauth/migration.rb
+++ lib/intuit-oauth/migration.rb
@@ -57,7 +57,7 @@ module IntuitOAuth
           client_secret: @client.secret
         }
 
-        IntuitOAuth::Transport.request('POST', migration_endpoint, headers, body)
+        IntuitOAuth::Transport.request('POST', migration_endpoint, headers, body.to_json)
       end
 
     end
