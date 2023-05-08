55309a1bca7d352da675ce41a272dc51181b4357
Nick <oswius@gmail.com>
2023-05-08 19:44:44 +0300

    Update constants.rb
    Change sandbox url for e2e

--- lib/intuit-oauth/constants.rb
+++ lib/intuit-oauth/constants.rb
@@ -1,6 +1,6 @@
 module IntuitOAuth
   class Config
-    DISCOVERY_URL_SANDBOX = 'https://developer.intuit.com/.well-known/openid_sandbox_configuration/'
+    DISCOVERY_URL_SANDBOX = 'https://developer-stage.intuit.com/.well-known/openid_configuration/'
     DISCOVERY_URL_PROD = 'https://developer.intuit.com/.well-known/openid_configuration/'
     MIGRATION_URL_SANDBOX = 'https://developer-sandbox.api.intuit.com/v2/oauth2/tokens/migrate'
     MIGRATION_URL_PROD = 'https://developer.api.intuit.com/v2/oauth2/tokens/migrate' 
